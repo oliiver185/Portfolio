c8139843f207200eb3a77b563cdd3ffc23463a71
oliiver185 <oliiver185@gmail.com>
2021-07-29 10:49:04 +0100

    adding country button

--- projectthree/script/myscript.js
+++ projectthree/script/myscript.js
@@ -1,5 +1,7 @@
 const search = document.getElementById('search'),
+    searchByCountry = document.getElementById('searchByCountry'),
     submit = document.getElementById('submit'),
+    submitByCountry = document.getElementById('submitByCountry'),
     random = document.getElementById('random'),
     mealsEl = document.getElementById('meals'),
     resultHeading = document.getElementById('result-heading'),
@@ -27,6 +29,8 @@ function searchMeal(e) {
                 console.log(data);
                 resultHeading.innerHTML = `<h2>Search results for '${term}':</h2>`
 
+
+
                 if (data.meals === null) {
                     resultHeading.innerHTML = `<h2>Sorry we  do not have any food with that name</h2>`
                 } else {
@@ -39,13 +43,61 @@ function searchMeal(e) {
                     </div>`).join(''); //usar join para volverlo string porque map() devuelve un array
                 }
             });
-
+        
         //clear search text
 
         search.value = '';
 
-    } else {
+    } else
+    {
         alert('PLease type a meal');
+    }
+
+
+}
+
+function searchMealByCountry (e) {
+    //prevent that we are not to submitting a file
+    e.preventDefault();
+
+    //clear single meal
+    single_mealEl.innerHTML = '';
+
+    //get serch term
+    const term = searchByCountry.value;
+    console.log(term);
+
+    //check for empty
+    if (term.trim()) {
+
+        fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${term}`)
+            .then(res => res.json())
+            .then(data => {
+                // console.log(data);
+                resultHeading.innerHTML = `<h2>Search results for '${term}':</h2>`
+
+
+
+                if (data.meals === null) {
+                    resultHeading.innerHTML = `<h2>Sorry we  do not have any food with that country name</h2>`
+                } else {
+                    mealsEl.innerHTML = data.meals.map(meal => `
+                    <div class="meal">
+                       <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+                        <div class="meal-info" data-mealID="${meal.idMeal}">
+                      <h3>${meal.strMeal}</h3>
+                        </div>
+                    </div>`).join(''); //usar join para volverlo string porque map() devuelve un array
+                }
+            });
+        
+        //clear search text
+
+        searchByCountry.value = '';
+
+    } else
+    {
+        alert('PLease type a country');
     }
 
 
@@ -56,26 +108,12 @@ function getMealById(mealID) {
     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
         .then(res => res.json())
         .then(data => {
-            console.log(data);
+            // console.log(data);
             const meal = data.meals[0];
 
             addMealtoDom(meal);
         });
 }
-
-
-function getArea(areaID) {
-    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${areaID}`)
-        .then(res => res.json()).then(data => {
-            console.log(data);
-            const area = data.meals[0]
-
-            // searchMeal(area);
-        });
-}
-
-
-
 
 
 
@@ -141,6 +179,7 @@ single_mealEl.innerHTML = `
 // event listeners
 
 submit.addEventListener('submit', searchMeal);
+submitByCountry.addEventListener('submit', searchMealByCountry);
 random.addEventListener('click', randomMeal);
 
 mealsEl.addEventListener('click', e => {
@@ -148,7 +187,7 @@ mealsEl.addEventListener('click', e => {
     // const mealInfo = e.composedPath().find(item => {
     const mealInfo = e.composedPath().find(item => {
 
-        console.log(item);
+        // console.log(item);
 
         if (item.classList) {
             return item.classList.contains('meal-info');
